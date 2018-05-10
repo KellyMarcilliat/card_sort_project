e0fdaa8f5842c03a0263576dab3a93f1e7a9995f
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-05-10 07:29:26 -0600

    Project Submittal:  card, guess, and deck files complete; round is WIP

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,14 @@
 require 'pry'
 
+#attr_reader :
+
 class Round
-  def initialize
-  end 
+  def initialize(deck_array)
+    @deck_array = deck_array
+  end
+
+  def guesses
+    #@deck_array
+  end
 
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -1,6 +1,7 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/round"
+require "./lib/deck"
 require "./lib/guess"
 require "./lib/card"
 require 'pry'
@@ -9,11 +10,25 @@ class RoundTest < Minitest::Test
   def test_round_exists
     card_1 = Card.new("3","Hearts")
     card_2 = Card.new("4", "Clubs")
-    #deck = Deck.new([card_1, card_2])
-    round = Round.new#(deck)
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
 
     assert_instance_of Round, round
-
-
   end
+
+  def test_deck_array_exists
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+
+    assert_equal [card_1, card_2], round.guesses
+  end
+
+  # QUESTION: Should I be testing whether deck exists as part of round_test?
+  # NOTE:  I still have some confusion over how to:
+          # - Interpret the spec
+          # - Design a series of tests of appropriate scope
+          # - Determine the right inputs for the methods driven by those tests
+
 end
